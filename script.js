effae8248d835ab3f1e59d9970c430ed581b6686
amansingh567 <122317805+amansingh567@users.noreply.github.com>
2024-08-20 18:31:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ btn.addEventListener("click",(e)=>{
     e.preventDefault();
     let value = document.querySelector("#city").value;
     let apikey = "e88543b6ac3a48a2ec7d1f42a0f7321e";
-    let apiurl = https://api.openweathermap.org/data/2.5/weather?units=metric&q=${value}&appid=${apikey};
+    let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${value}&appid=${apikey}`;
     fetch(apiurl)
     .then(res => res.json())
     .then(data => {
@@ -12,7 +12,7 @@ btn.addEventListener("click",(e)=>{
             Notification.requestPermission().then(per =>{
                 if (per === "granted") {
                     let noti = new Notification("weather notification", {
-                        body:description : ${data.weather[0].description},\ntemp :${data.main.temp} celcius,\nhumidity : ${data.main.humidity} g/m3,\npressure : ${data.main.pressure} mbar,\nspeed : ${data.wind.speed} m/s ,\nmax temperature : ${data.main.temp_max} celcius,\nmin temperature : ${data.main.temp_min} celcius
+                        body:`description : ${data.weather[0].description},\ntemp :${data.main.temp} celcius,\nhumidity : ${data.main.humidity} g/m3,\npressure : ${data.main.pressure} mbar,\nspeed : ${data.wind.speed} m/s ,\nmax temperature : ${data.main.temp_max} celcius,\nmin temperature : ${data.main.temp_min} celcius`
                     })
                 }else{
                     alert("permission not granted");
@@ -22,7 +22,7 @@ btn.addEventListener("click",(e)=>{
             Notification.requestPermission().then(per =>{
                 if (per === "granted") {
                     let noti = new Notification("weather notification", {
-                        body:city not exist
+                        body:`city not exist`
                     })
                 }else{
                     alert("city not exist")
@@ -33,3 +33,6 @@ btn.addEventListener("click",(e)=>{
     })
     
 })
+
+
+
